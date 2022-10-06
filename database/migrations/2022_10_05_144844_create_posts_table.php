20c2d0070686d6221a0dbd41c893654c08cf8131
Aaron Saula <n00201123@iadt.ie>
2022-10-06 12:00:44 +0100

    Migration added

--- database/migrations/2022_10_05_144844_create_posts_table.php
+++ database/migrations/2022_10_05_144844_create_posts_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('op');
+            $table->string('date');
+            $table->integer('likes');
             $table->timestamps();
         });
     }
