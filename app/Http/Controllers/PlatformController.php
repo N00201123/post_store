720137e69e2e547b2764796122f1128197bc5758
Aaron Saula <n00201123@iadt.ie>
2022-12-07 11:43:47 +0000

    TagsUpdatedPt1

--- app/Http/Controllers/PlatformController.php
+++ app/Http/Controllers/PlatformController.php
@@ -16,6 +16,24 @@ class PlatformController extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @OA\Get(
+     *     path="/api/platforms",
+     *     description="Displays all the platforms",
+     *     tags={"Platforms"},
+     *      @OA\Response(
+        *          response=200,
+        *          description="Successful operation, Returns a list of Posts in JSON format"
+        *       ),
+        *      @OA\Response(
+        *          response=401,
+        *          description="Unauthenticated",
+        *      ),
+        *      @OA\Response(
+        *          response=403,
+        *          description="Forbidden"
+        *      )
+ * )
+     * 
      * @return \Illuminate\Http\Response
      */
     public function index()
@@ -25,7 +43,30 @@ class PlatformController extends Controller
 
     /**
      * Store a newly created resource in storage.
-     *
+     * 
+     * @OA\Post(
+     *      path="/api/platforms",
+     *      operationId="storePlatform",
+     *      tags={"Platforms"},
+     *      summary="Create a new Platform",
+     *      description="Stores the platform in the DB",
+     *      @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *            required={"name", "description"},
+     *            @OA\Property(property="name", type="string", format="string", example="Sample Name"),
+     *            @OA\Property(property="description", type="string", format="string", example="A long description about this great post")
+     *      )
+     *      ),
+     *     @OA\Response(
+     *          response=200, description="Success",
+     *          @OA\JsonContent(
+     *             @OA\Property(property="status", type="integer", example=""),
+     *             @OA\Property(property="data",type="object")
+     *          )
+     *     )
+     * )
+     * 
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
@@ -41,7 +82,31 @@ class PlatformController extends Controller
 
     /**
      * Display the specified resource.
-     *
+     * @OA\Get(
+    *     path="/api/platforms/{id}",
+    *     description="Gets a platform by ID",
+    *     tags={"Platforms"},
+    *          @OA\Parameter(
+        *          name="id",
+        *          description="Platform id",
+        *          required=true,
+        *          in="path",
+        *          @OA\Schema(
+        *              type="integer")
+     *          ),
+        *      @OA\Response(
+        *          response=200,
+        *          description="Successful operation"
+        *       ),
+        *      @OA\Response(
+        *          response=401,
+        *          description="Unauthenticated",
+        *      ),
+        *      @OA\Response(
+        *          response=403,
+        *          description="Forbidden"
+        *      )
+*)
      * @param  \App\Models\Platform  $platform
      * @return \Illuminate\Http\Response
      */
@@ -63,6 +128,28 @@ class PlatformController extends Controller
     }
 
     /**
+     *
+     *
+     * @OA\Delete(
+     *    path="/api/platforms/{id}",
+     *    operationId="destroyPlatform",
+     *    tags={"Platforms"},
+     *    summary="Delete a Platform",
+     *    description="Delete Platform",
+     *    @OA\Parameter(name="id", in="path", description="Id of a Platform", required=true,
+     *        @OA\Schema(type="integer")
+     *    ),
+     *    @OA\Response(
+     *         response=Response::HTTP_NO_CONTENT,
+     *         description="Success",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status_code", type="integer", example="204"),
+     *         @OA\Property(property="data",type="object")
+     *          ),
+     *       )
+     *      )
+     *  )
+
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Platform  $platform
@@ -70,6 +157,6 @@ class PlatformController extends Controller
      */
     public function destroy(Platform $platform)
     {
-        //
+        $platform->delete();
     }
 }
